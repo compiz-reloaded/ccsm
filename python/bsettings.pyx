6ffcc102607247cff3bfef12cfcfb9ea0645c634
Quinn Storm <quinn@darter.(none)>
2007-04-24 12:32:45 -0400

    adding simple bsettings.pyx file to get started

---
+++ python/bsettings.pyx
@@ -0,0 +1,31 @@
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+.
+#
+# Authors: Quinn Storm (quinn@beryl-project.org)
+# Copyright (C) 2007 Quinn Storm
+
+cdef extern struct BSContext:
+
+cdef extern BSContext * bsContextNew()
+cdef extern void bsContextDestroy(BSContext * context)
+
+cdef class Context:
+	cdef BSContext * bsContext
+
+	def __new__(self):
+		self.bsContext=bsContextNew()
+
+	def __dealloc__(self):
+		bsContextDestroy(self.bsContext)
