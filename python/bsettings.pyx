4478b7174be1a94d2906f124e07073c67599e2b1
Quinn Storm <quinn@darter.(none)>
2007-04-24 13:28:45 -0400

    getting started...

--- python/bsettings.pyx
+++ python/bsettings.pyx
@@ -15,16 +15,75 @@
 # Authors: Quinn Storm (quinn@beryl-project.org)
 # Copyright (C) 2007 Quinn Storm
 
-cdef extern struct BSContext
+cdef struct BSPlugin
+
+cdef struct BSPluginList:
+	BSPlugin * data
+
+cdef extern struct BSStringList
+cdef extern struct BSSettingList
+cdef extern struct BSGroupList
+
+cdef struct BSContext:
+	BSPluginList * plugins
+
+cdef struct BSPlugin:
+	char * name
+	char * shortDesc
+	char * longDesc
+	char * hints
+	char * category
+	char * filename
+	BSStringList * loadAfter
+	BSStringList * loadBefore
+	BSStringList * requiresPlugin
+	BSStringList * providesFeature
+	BSStringList * requiresFeature
+	BSSettingList * settings
+	BSGroupList * groups
+	void * priv
+	BSContext * context
+
 
 cdef extern BSContext * bsContextNew()
 cdef extern void bsContextDestroy(BSContext * context)
+cdef extern unsigned int bsPluginListLength(BSPluginList * list)
+cdef extern BSPluginList * bsPluginListGetItem(BSPluginList * list, unsigned int idx)
+cdef extern BSPlugin * bsFindPlugin(BSContext * context, char * name)
+
+cdef class Context
+
+cdef class Plugin:
+	cdef BSPlugin * bsPlugin
+	
+	def __new__(self,Context context,name):
+		self.bsPlugin = bsFindPlugin(context.bsContext,name)
+	
+	property Name:
+		def __get__(self):
+			return self.bsPlugin.name
+	property ShortDesc:
+		def __get__(self):
+			return self.bsPlugin.shortDesc
+	property LongDesc:
+		def __get__(self):
+			return self.bsPlugin.longDesc
 
 cdef class Context:
 	cdef BSContext * bsContext
+	cdef object plugins
 
 	def __new__(self):
+		cdef BSPlugin * pl
+		self.plugins={}
 		self.bsContext=bsContextNew()
+		for n in range(0,bsPluginListLength(self.bsContext.plugins)):
+			pl = bsPluginListGetItem(self.bsContext.plugins,n).data
+			self.plugins[pl.name]=Plugin(self,pl.name)
 
 	def __dealloc__(self):
 		bsContextDestroy(self.bsContext)
+
+	property Plugins:
+		def __get__(self):
+			return self.plugins
