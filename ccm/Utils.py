273fa486f12ce7c0e05eee8fe2843cd373735c52
Guillaume Seguin <guillaume@segu.in>
2007-08-22 16:42:24 +0200

    * Python 4 spaces tabs indent

--- ccm/Utils.py
+++ ccm/Utils.py
@@ -36,207 +36,207 @@ gettext.textdomain("ccsm")
 _ = gettext.gettext
 
 def getScreens():
-	screens = []
-	display = gtk.gdk.display_get_default()
-	nScreens = display.get_n_screens()
-	for i in range(nScreens):
-		screens.append(i)
-	return screens
+    screens = []
+    display = gtk.gdk.display_get_default()
+    nScreens = display.get_n_screens()
+    for i in range(nScreens):
+        screens.append(i)
+    return screens
 
 class Style:
-	def __init__(self):
-		fakeWindow = gtk.Window()
-		styleWidget = gtk.Entry()
-		fakeWindow.add(styleWidget)
-		styleWidget.realize()
-		bc = styleWidget.style.bg[gtk.STATE_SELECTED]
-		self.BrightColor = "#%.4x%.4x%.4x" % (bc.red, bc.green, bc.blue)
-		bc = styleWidget.style.light[gtk.STATE_NORMAL]
-		self.BackgroundColor = "#%.4x%.4x%.4x" % (bc.red, bc.green, bc.blue)
-		styleWidget.destroy()
-		fakeWindow.destroy()
+    def __init__(self):
+        fakeWindow = gtk.Window()
+        styleWidget = gtk.Entry()
+        fakeWindow.add(styleWidget)
+        styleWidget.realize()
+        bc = styleWidget.style.bg[gtk.STATE_SELECTED]
+        self.BrightColor = "#%.4x%.4x%.4x" % (bc.red, bc.green, bc.blue)
+        bc = styleWidget.style.light[gtk.STATE_NORMAL]
+        self.BackgroundColor = "#%.4x%.4x%.4x" % (bc.red, bc.green, bc.blue)
+        styleWidget.destroy()
+        fakeWindow.destroy()
 
 class Image(gtk.Image):
-	def __init__(self, name=None, type=ImageNone, size = 32):
-		gtk.Image.__init__(self)
+    def __init__(self, name=None, type=ImageNone, size = 32):
+        gtk.Image.__init__(self)
 
-		if name != None:
-			if type == ImagePlugin:
-				iconpath = "%s/plugin-%s.svg" % (PixmapDir, name)
-				if not os.path.exists(iconpath):
-					iconpath = "%s/plugin-unknown.svg"%PixmapDir
-				try:
-					pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(iconpath, size, size)
-					self.set_from_pixbuf(pixbuf)
-				except:
-					self.set_from_stock(gtk.STOCK_MISSING_IMAGE, gtk.ICON_SIZE_BUTTON)
+        if name != None:
+            if type == ImagePlugin:
+                iconpath = "%s/plugin-%s.svg" % (PixmapDir, name)
+                if not os.path.exists(iconpath):
+                    iconpath = "%s/plugin-unknown.svg"%PixmapDir
+                try:
+                    pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(iconpath, size, size)
+                    self.set_from_pixbuf(pixbuf)
+                except:
+                    self.set_from_stock(gtk.STOCK_MISSING_IMAGE, gtk.ICON_SIZE_BUTTON)
 
-			elif type == ImageCategory:
-				iconpath = "%s/category-%s.svg" % (PixmapDir, name)
-				if not os.path.exists(iconpath):
-					iconpath = "%s/category-uncategorized.svg" % PixmapDir
-				try:
-					pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(iconpath, size, size)
-					self.set_from_pixbuf(pixbuf)
-				except:
-					self.set_from_stock(gtk.STOCK_MISSING_IMAGE, gtk.ICON_SIZE_BUTTON)
+            elif type == ImageCategory:
+                iconpath = "%s/category-%s.svg" % (PixmapDir, name)
+                if not os.path.exists(iconpath):
+                    iconpath = "%s/category-uncategorized.svg" % PixmapDir
+                try:
+                    pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(iconpath, size, size)
+                    self.set_from_pixbuf(pixbuf)
+                except:
+                    self.set_from_stock(gtk.STOCK_MISSING_IMAGE, gtk.ICON_SIZE_BUTTON)
 
-			elif type == ImageThemed:
-				iconTheme = gtk.icon_theme_get_default()
-				try:
-					pixbuf = iconTheme.load_icon(name, size, 0)
-					self.set_from_pixbuf(pixbuf)
-				except:
-					self.set_from_stock(gtk.STOCK_MISSING_IMAGE, gtk.ICON_SIZE_BUTTON)
+            elif type == ImageThemed:
+                iconTheme = gtk.icon_theme_get_default()
+                try:
+                    pixbuf = iconTheme.load_icon(name, size, 0)
+                    self.set_from_pixbuf(pixbuf)
+                except:
+                    self.set_from_stock(gtk.STOCK_MISSING_IMAGE, gtk.ICON_SIZE_BUTTON)
 
-			elif type == ImageStock:
-				try:
-					self.set_from_stock(name, size)
-				except:
-					self.set_from_stock(gtk.STOCK_MISSING_IMAGE, gtk.ICON_SIZE_BUTTON)
+            elif type == ImageStock:
+                try:
+                    self.set_from_stock(name, size)
+                except:
+                    self.set_from_stock(gtk.STOCK_MISSING_IMAGE, gtk.ICON_SIZE_BUTTON)
 
 class Label(gtk.Label):
-	def __init__(self, value = "", wrap = 160):
-		gtk.Label.__init__(self, value)
-		self.props.xalign = 0
-		self.props.wrap_mode = gtk.WRAP_WORD
-		self.set_line_wrap(True)
-		self.set_size_request(wrap, -1)
+    def __init__(self, value = "", wrap = 160):
+        gtk.Label.__init__(self, value)
+        self.props.xalign = 0
+        self.props.wrap_mode = gtk.WRAP_WORD
+        self.set_line_wrap(True)
+        self.set_size_request(wrap, -1)
 
 class NotFoundBox(gtk.Alignment):
-	def __init__(self, value):
-		gtk.Alignment.__init__(self, 0.5, 0.5, 0.0, 0.0)
-		
-		box = gtk.HBox()
-		self.Warning = gtk.Label()
-		self.Markup = _("<span size=\"large\"><b>No matches found.</b> </span><span>\n\n Your filter \"<b>%s</b>\" does not match any items.</span>")
-		self.Warning.set_markup(self.Markup % value)
-		image = Image("face-surprise", ImageThemed, 48)
-			
-		box.pack_start(image, False, False, 0)
-		box.pack_start(self.Warning, True, True, 15)
-		self.add(box)
+    def __init__(self, value):
+        gtk.Alignment.__init__(self, 0.5, 0.5, 0.0, 0.0)
+        
+        box = gtk.HBox()
+        self.Warning = gtk.Label()
+        self.Markup = _("<span size=\"large\"><b>No matches found.</b> </span><span>\n\n Your filter \"<b>%s</b>\" does not match any items.</span>")
+        self.Warning.set_markup(self.Markup % value)
+        image = Image("face-surprise", ImageThemed, 48)
+            
+        box.pack_start(image, False, False, 0)
+        box.pack_start(self.Warning, True, True, 15)
+        self.add(box)
 
-	def update(self, value):
-		self.Warning.set_markup(self.Markup % value)
+    def update(self, value):
+        self.Warning.set_markup(self.Markup % value)
 
 # Updates all registered setting when they where changed through CompizConfig
 class Updater:
-	def __init__(self, context):
-		self.VisibleSettings = []
-		self.NotRemoved = []
-		self.Context = context
+    def __init__(self, context):
+        self.VisibleSettings = []
+        self.NotRemoved = []
+        self.Context = context
 
-		gobject.timeout_add(2000, self.Update)
-	
-	def Append(self, setting):
-		self.VisibleSettings.append(setting)
+        gobject.timeout_add(2000, self.Update)
+    
+    def Append(self, setting):
+        self.VisibleSettings.append(setting)
 
-	def Update(self):
-		changed = self.Context.ProcessEvents()
-		if changed:
-			changedSettings = self.Context.ChangedSettings
-			for settingWidget in self.VisibleSettings:
-				# Remove already destroyed widgets
-				if not settingWidget.Widget.get_parent():
-					self.VisibleSettings.remove(settingWidget)
-				
-				# Exception for multi settings widgets (multi list widget, action page, etc.)
-				if settingWidget.Setting.__class__ != list:
-					if settingWidget.Setting in changedSettings:
-						settingWidget.Read()
-						changedSettings.remove(settingWidget.Setting)
-				else:
-					read = False
-					for setting in settingWidget.Setting:
-						if setting in changedSettings:
-							read = True
-							changedSettings.remove(setting)
-					if read:
-						settingWidget.Read()
-			# For removing non-visible settings
-			for oldSetting in self.NotRemoved:
-				if oldSetting in changedSettings:
-					changedSettings.remove(oldSetting)
-			self.NotRemoved = changedSettings
-			self.Context.ChangedSettings = changedSettings
+    def Update(self):
+        changed = self.Context.ProcessEvents()
+        if changed:
+            changedSettings = self.Context.ChangedSettings
+            for settingWidget in self.VisibleSettings:
+                # Remove already destroyed widgets
+                if not settingWidget.Widget.get_parent():
+                    self.VisibleSettings.remove(settingWidget)
+                
+                # Exception for multi settings widgets (multi list widget, action page, etc.)
+                if settingWidget.Setting.__class__ != list:
+                    if settingWidget.Setting in changedSettings:
+                        settingWidget.Read()
+                        changedSettings.remove(settingWidget.Setting)
+                else:
+                    read = False
+                    for setting in settingWidget.Setting:
+                        if setting in changedSettings:
+                            read = True
+                            changedSettings.remove(setting)
+                    if read:
+                        settingWidget.Read()
+            # For removing non-visible settings
+            for oldSetting in self.NotRemoved:
+                if oldSetting in changedSettings:
+                    changedSettings.remove(oldSetting)
+            self.NotRemoved = changedSettings
+            self.Context.ChangedSettings = changedSettings
 
-		return True
+        return True
 
 class PureVirtualError(Exception):
-	pass
+    pass
 
 def SettingSortCompare(v1, v2):
-	return cmp(v1.Plugin.Ranking[v1.Name], v2.Plugin.Ranking[v2.Name])
+    return cmp(v1.Plugin.Ranking[v1.Name], v2.Plugin.Ranking[v2.Name])
 
 def FirstItemSortCompare(sg1, sg2):
-	return cmp(sg1[0], sg2[0])
+    return cmp(sg1[0], sg2[0])
 
 def EnumSettingSortCompare(v1, v2):
-	return cmp(v1[1], v2[1])
+    return cmp(v1[1], v2[1])
 
 def PluginSortCompare(p1, p2):
-	return cmp(p1.ShortDesc, p2.ShortDesc)
+    return cmp(p1.ShortDesc, p2.ShortDesc)
 
 # singleRun is used to combine the run stages, in this case run is a list
 # containing the run levels which should be used to filter the settings
 def FilterSettings(settings, filter, run=0, noActions=False, singleRun=False):
-	if filter == None:
-		return settings
+    if filter == None:
+        return settings
 
-	filteredSettings = []
+    filteredSettings = []
 
-	for setting in settings:
-		if noActions and setting.Type == 'Action':
-			continue
-		# First run, only search in shortDesc and name
-		if run == 0 or (singleRun and run.count(0) != 0):
-			shortDesc = setting.ShortDesc.lower()
-			name = setting.Name.lower()
-			if shortDesc.find(filter) != -1:
-				filteredSettings.append(setting)
-				continue
-			elif name.find(filter) != -1:
-				filteredSettings.append(setting)
-				continue
-		# Then in longDesc
-		if run == 1 or (singleRun and run.count(1) != 0):
-			longDesc = setting.LongDesc.lower()
-			if longDesc.find(filter) != -1:
-				filteredSettings.append(setting)
-				continue
-		# Finally search in the option value
-		if run == 2 or (singleRun and run.count(2) != 0):
-			value = ""
-			# make sure enum settings work too
-			if setting.Type == 'Int' and len(setting.Info[2].keys()) > 0:
-					value = sorted(setting.Info[2].items(), EnumSettingSortCompare)[setting.Value][0]
-					value = value.lower()
-			# also make sure intDesc settings work right
-			elif setting.Type == 'List' and setting.Info[0] == 'Int' and len(setting.Info[1][2]) > 0:
-				for int in setting.Value:
-					for item in setting.Info[1][2].items():
-						if item[1] == int:
-							value += item[0]
-				value = value.lower()
-			# we also need actions support
-			elif setting.Type == 'Action':
-				value = str(setting.Value).lower()
-			else:
-				value = str(setting.Value).lower()
-			if value.find(filter) != -1:
-				filteredSettings.append(setting)
+    for setting in settings:
+        if noActions and setting.Type == 'Action':
+            continue
+        # First run, only search in shortDesc and name
+        if run == 0 or (singleRun and run.count(0) != 0):
+            shortDesc = setting.ShortDesc.lower()
+            name = setting.Name.lower()
+            if shortDesc.find(filter) != -1:
+                filteredSettings.append(setting)
+                continue
+            elif name.find(filter) != -1:
+                filteredSettings.append(setting)
+                continue
+        # Then in longDesc
+        if run == 1 or (singleRun and run.count(1) != 0):
+            longDesc = setting.LongDesc.lower()
+            if longDesc.find(filter) != -1:
+                filteredSettings.append(setting)
+                continue
+        # Finally search in the option value
+        if run == 2 or (singleRun and run.count(2) != 0):
+            value = ""
+            # make sure enum settings work too
+            if setting.Type == 'Int' and len(setting.Info[2].keys()) > 0:
+                    value = sorted(setting.Info[2].items(), EnumSettingSortCompare)[setting.Value][0]
+                    value = value.lower()
+            # also make sure intDesc settings work right
+            elif setting.Type == 'List' and setting.Info[0] == 'Int' and len(setting.Info[1][2]) > 0:
+                for int in setting.Value:
+                    for item in setting.Info[1][2].items():
+                        if item[1] == int:
+                            value += item[0]
+                value = value.lower()
+            # we also need actions support
+            elif setting.Type == 'Action':
+                value = str(setting.Value).lower()
+            else:
+                value = str(setting.Value).lower()
+            if value.find(filter) != -1:
+                filteredSettings.append(setting)
 
-	# Nothing was found, search also in the longDesc/value
-	if len(filteredSettings) == 0 and run < 2 and not singleRun:
-		return FilterSettings(settings, filter, run+1, noActions, False)
+    # Nothing was found, search also in the longDesc/value
+    if len(filteredSettings) == 0 and run < 2 and not singleRun:
+        return FilterSettings(settings, filter, run+1, noActions, False)
 
-	return filteredSettings
+    return filteredSettings
 
 def HasOnlyType(settings, type):
-	empty = True
-	for setting in settings:
-		empty = False
-		if setting.Type != type:
-			return False
-	return not empty
+    empty = True
+    for setting in settings:
+        empty = False
+        if setting.Type != type:
+            return False
+    return not empty
--- ccm/Widgets.py
+++ ccm/Widgets.py
@@ -41,174 +41,174 @@ _ = gettext.gettext
 # Selector Buttons
 #
 class SelectorButtons(gtk.HBox):
-	def __init__(self):
-		gtk.HBox.__init__(self)
-		self.set_border_width(10)
-		self.set_spacing(5)
-		self.buttons = []
-		self.arrows = []
+    def __init__(self):
+        gtk.HBox.__init__(self)
+        self.set_border_width(10)
+        self.set_spacing(5)
+        self.buttons = []
+        self.arrows = []
 
-	def clear_buttons(self):
-		for widget in (self.arrows + self.buttons):
-			widget.destroy()
+    def clear_buttons(self):
+        for widget in (self.arrows + self.buttons):
+            widget.destroy()
 
-		self.arrows = []
-		self.buttons = []
+        self.arrows = []
+        self.buttons = []
 
-	def add_button(self, label, callback):
-		arrow = gtk.Arrow(gtk.ARROW_RIGHT, gtk.SHADOW_NONE)
-		button = gtk.Button(label)
-		button.set_relief(gtk.RELIEF_NONE)
-		button.connect('clicked', callback, label)
-		if len(self.get_children()) > 0:
-			self.pack_start(arrow, False, False)
-			self.arrows.append(arrow)
-		self.pack_start(button, False, False)
-		self.buttons.append(button)
-		self.show_all()
+    def add_button(self, label, callback):
+        arrow = gtk.Arrow(gtk.ARROW_RIGHT, gtk.SHADOW_NONE)
+        button = gtk.Button(label)
+        button.set_relief(gtk.RELIEF_NONE)
+        button.connect('clicked', callback, label)
+        if len(self.get_children()) > 0:
+            self.pack_start(arrow, False, False)
+            self.arrows.append(arrow)
+        self.pack_start(button, False, False)
+        self.buttons.append(button)
+        self.show_all()
 
-	def remove_button(self, pos):
-		if pos > len(self.buttons)-1:
-			return
-		self.buttons[pos].destroy()
-		self.buttons.remove(self.buttons[pos])
-		if pos > 0:
-			self.arrows[pos-1].destroy()
-			self.arrows.remove(self.arrows[pos-1])
+    def remove_button(self, pos):
+        if pos > len(self.buttons)-1:
+            return
+        self.buttons[pos].destroy()
+        self.buttons.remove(self.buttons[pos])
+        if pos > 0:
+            self.arrows[pos-1].destroy()
+            self.arrows.remove(self.arrows[pos-1])
 
 # Selector Box
 #
 class SelectorBox(gtk.ScrolledWindow):
-	def __init__(self, backgroundColor):
-		gtk.ScrolledWindow.__init__(self)
-		self.viewport = gtk.Viewport()
-		self.viewport.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse(backgroundColor))
-		self.props.hscrollbar_policy = gtk.POLICY_NEVER
-		self.props.vscrollbar_policy = gtk.POLICY_AUTOMATIC
-		self.set_size_request(210, 150)
-		self.box = gtk.VBox()
-		self.box.set_spacing(5)
-		self.viewport.add(self.box)
-		self.add(self.viewport)
+    def __init__(self, backgroundColor):
+        gtk.ScrolledWindow.__init__(self)
+        self.viewport = gtk.Viewport()
+        self.viewport.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse(backgroundColor))
+        self.props.hscrollbar_policy = gtk.POLICY_NEVER
+        self.props.vscrollbar_policy = gtk.POLICY_AUTOMATIC
+        self.set_size_request(210, 150)
+        self.box = gtk.VBox()
+        self.box.set_spacing(5)
+        self.viewport.add(self.box)
+        self.add(self.viewport)
 
-	def close(self):
-		self.destroy()
-		self.viewport.destroy()
-		for button in self.box.get_children():
-			button.destroy()
-		self.box.destroy()
+    def close(self):
+        self.destroy()
+        self.viewport.destroy()
+        for button in self.box.get_children():
+            button.destroy()
+        self.box.destroy()
 
-	def add_item(self, item, callback, markup="%s"):
-		button = gtk.Button()
-		label = Label()
-		item = item.replace("&", "&amp;")
-		label.set_markup(markup % item or _("General"))
-		button.add(label)
-		button.connect("clicked", callback, item)
-		button.set_relief(gtk.RELIEF_NONE)
-		self.box.pack_start(button, False, False)
+    def add_item(self, item, callback, markup="%s"):
+        button = gtk.Button()
+        label = Label()
+        item = item.replace("&", "&amp;")
+        label.set_markup(markup % item or _("General"))
+        button.add(label)
+        button.connect("clicked", callback, item)
+        button.set_relief(gtk.RELIEF_NONE)
+        self.box.pack_start(button, False, False)
 
-	def clear_list(self):
-		for button in self.box.get_children():
-			button.destroy()
-	
-	def set_item_list(self, list, callback):
-		self.clear_list()
-		for item in list:
-			self.add_item(item)
-			
-		self.box.show_all()
+    def clear_list(self):
+        for button in self.box.get_children():
+            button.destroy()
+    
+    def set_item_list(self, list, callback):
+        self.clear_list()
+        for item in list:
+            self.add_item(item)
+            
+        self.box.show_all()
 
 # Scrolled List
 #
 class ScrolledList(gtk.ScrolledWindow):
-	def __init__(self, name):
-		gtk.ScrolledWindow.__init__(self)
+    def __init__(self, name):
+        gtk.ScrolledWindow.__init__(self)
 
-		self.props.hscrollbar_policy = gtk.POLICY_NEVER
-		self.props.vscrollbar_policy = gtk.POLICY_AUTOMATIC
+        self.props.hscrollbar_policy = gtk.POLICY_NEVER
+        self.props.vscrollbar_policy = gtk.POLICY_AUTOMATIC
 
-		self.store = gtk.ListStore(gobject.TYPE_STRING)
+        self.store = gtk.ListStore(gobject.TYPE_STRING)
 
-		self.custom_style = Style()
+        self.custom_style = Style()
 
-		viewport = gtk.Viewport()
-		viewport.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse(self.custom_style.BackgroundColor))
-	
-		self.view = gtk.TreeView(self.store)
-		self.view.set_headers_visible(True)
-		self.view.insert_column_with_attributes(-1, name, gtk.CellRendererText(), text=0)
-		
-		self.set_size_request(300, 300)
-		
-		viewport.add(self.view)
-		self.add(viewport)
-		
-		self.select = self.view.get_selection()
-		self.select.set_mode(gtk.SELECTION_SINGLE)
+        viewport = gtk.Viewport()
+        viewport.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse(self.custom_style.BackgroundColor))
+    
+        self.view = gtk.TreeView(self.store)
+        self.view.set_headers_visible(True)
+        self.view.insert_column_with_attributes(-1, name, gtk.CellRendererText(), text=0)
+        
+        self.set_size_request(300, 300)
+        
+        viewport.add(self.view)
+        self.add(viewport)
+        
+        self.select = self.view.get_selection()
+        self.select.set_mode(gtk.SELECTION_SINGLE)
 
-	def get_list(self):
-		values = []
-		iter = self.store.get_iter_first()
-		while iter:
-			value = self.store.get(iter, 0)[0]
-			if value != "":
-				values.append(value)
-			iter = self.store.iter_next(iter)	
-		return values
+    def get_list(self):
+        values = []
+        iter = self.store.get_iter_first()
+        while iter:
+            value = self.store.get(iter, 0)[0]
+            if value != "":
+                values.append(value)
+            iter = self.store.iter_next(iter)    
+        return values
 
-	def clear(self):
-		self.store.clear()
-	
-	def append(self, value):
-		iter = self.store.append()
-		self.store.set(iter, 0, value)
+    def clear(self):
+        self.store.clear()
+    
+    def append(self, value):
+        iter = self.store.append()
+        self.store.set(iter, 0, value)
 
-	def set(self, pos, value):
-		iter = self.store.get_iter(pos)
-		self.store.set(iter, 0, value)
+    def set(self, pos, value):
+        iter = self.store.get_iter(pos)
+        self.store.set(iter, 0, value)
 
-	def delete(self, b):
-		selected_rows = self.select.get_selected_rows()[1]
-		for path in selected_rows:
-			iter = self.store.get_iter(path)
-			self.store.remove(iter)
-	
-	def move_up(self, b):
-		selected_rows = self.select.get_selected_rows()[1]
-		if len(selected_rows) == 1:
-			iter = self.store.get_iter(selected_rows[0])
-			prev = self.store.get_iter_first()
-			if not self.store.get_path(prev) == self.store.get_path(iter):
-				while prev is not None and not self.store.get_path(self.store.iter_next(prev)) == self.store.get_path(iter):
-					prev = self.store.iter_next(prev)
-				self.store.swap(iter, prev)
+    def delete(self, b):
+        selected_rows = self.select.get_selected_rows()[1]
+        for path in selected_rows:
+            iter = self.store.get_iter(path)
+            self.store.remove(iter)
+    
+    def move_up(self, b):
+        selected_rows = self.select.get_selected_rows()[1]
+        if len(selected_rows) == 1:
+            iter = self.store.get_iter(selected_rows[0])
+            prev = self.store.get_iter_first()
+            if not self.store.get_path(prev) == self.store.get_path(iter):
+                while prev is not None and not self.store.get_path(self.store.iter_next(prev)) == self.store.get_path(iter):
+                    prev = self.store.iter_next(prev)
+                self.store.swap(iter, prev)
 
-	def move_down(self, b):
-		selected_rows = self.select.get_selected_rows()[1]
-		if len(selected_rows) == 1:
-			iter = self.store.get_iter(selected_rows[0])
-			next = self.store.iter_next(iter)
-			if next is not None:
-				self.store.swap(iter, next)
+    def move_down(self, b):
+        selected_rows = self.select.get_selected_rows()[1]
+        if len(selected_rows) == 1:
+            iter = self.store.get_iter(selected_rows[0])
+            next = self.store.iter_next(iter)
+            if next is not None:
+                self.store.swap(iter, next)
 
 # About Dialog
 #
 class AboutDialog (gtk.AboutDialog):
-	def __init__ (self):
-		gtk.AboutDialog.__init__ (self)
+    def __init__ (self):
+        gtk.AboutDialog.__init__ (self)
 
-		self.set_name (_("CompizConfig Settings Manager"))
-		self.set_version (Version)
-		self.set_comments (_("This is a settings manager for the CompizConfig configuration system."))
-		self.set_copyright ("Copyright \xC2\xA9 2007 Patrick Niklaus/Quinn Storm")
-		self.set_translator_credits (_("translator-credits"))
-		self.set_authors (["Patrick Niklaus <marex@opencompositing.org>",
-						   "Quinn Storm <quinn@beryl-project.org>"])
-		self.set_artists (["Andrew Wedderburn <andrew.wedderburn@gmail.com>",
-						   "Patrick Niklaus <marex@opencompositing.org>",
-						   "Gnome Icon Theme Team"])
-		self.set_icon (gtk.gdk.pixbuf_new_from_file (IconDir+"/apps/ccsm.svg"))
-		self.set_logo (gtk.gdk.pixbuf_new_from_file (IconDir+"/apps/ccsm.svg"))
-		self.set_website ("http://www.compiz-fusion.org")
+        self.set_name (_("CompizConfig Settings Manager"))
+        self.set_version (Version)
+        self.set_comments (_("This is a settings manager for the CompizConfig configuration system."))
+        self.set_copyright ("Copyright \xC2\xA9 2007 Patrick Niklaus/Quinn Storm")
+        self.set_translator_credits (_("translator-credits"))
+        self.set_authors (["Patrick Niklaus <marex@opencompositing.org>",
+                           "Quinn Storm <quinn@beryl-project.org>"])
+        self.set_artists (["Andrew Wedderburn <andrew.wedderburn@gmail.com>",
+                           "Patrick Niklaus <marex@opencompositing.org>",
+                           "Gnome Icon Theme Team"])
+        self.set_icon (gtk.gdk.pixbuf_new_from_file (IconDir+"/apps/ccsm.svg"))
+        self.set_logo (gtk.gdk.pixbuf_new_from_file (IconDir+"/apps/ccsm.svg"))
+        self.set_website ("http://www.compiz-fusion.org")
 
