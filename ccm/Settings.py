d1529f6703186d2adebbf5d72c9fb35d64f00056
Chris Townsend <christopher.townsend@canonical.com>
2014-04-11 11:37:49 -0400

    Explicitely set the value after the SpinButton is created
    Due to some change in Gtk SpinButton, setting the initial value in
    Adjustment does not work for integers.

--- ccm/Settings.py
+++ ccm/Settings.py
@@ -495,8 +495,9 @@ class NumberSetting(StockSetting):
             self.Inc = info[2]
         inc = self.Inc
         self.NoneValue = info[0]
-        self.Adj = gtk.Adjustment(self.Get(), info[0], info[1], inc, inc*10)
+        self.Adj = gtk.Adjustment(0, info[0], info[1], inc, inc*10)
         self.Spin = gtk.SpinButton(self.Adj)
+        self.Spin.set_value(self.Get())
 
         self.Scale = gtk.HScale(self.Adj)
 
